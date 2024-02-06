e19221b1b9f63963f45ab6004ca1de23903e762e
1ris_W <ediath462@gmail.com>
2024-02-07 01:56:22 +0800

    init repo

---
+++ PiliPili/PiliPiliApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiliPiliApp.swift
+//  PiliPili
+//
+//  Created by Iris on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PiliPiliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
